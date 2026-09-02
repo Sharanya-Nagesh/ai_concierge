0bb16cc0558e38770e84d8a14cd31dfe9e7673b9
Sharanya-Nagesh <69107485+Sharanya-Nagesh@users.noreply.github.com>
2026-09-02 10:48:17 +0530

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 from app.models.user import User
-from app.models.user_preferences import UserPreference
+from app.models.user_preference import UserPreference
 from app.models.conversation import Conversation
 from app.models.message import Message
 from app.models.memory import Memory
